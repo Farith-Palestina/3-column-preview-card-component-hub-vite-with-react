aa69fa17f9a701c0777a92a6a28f3a1a7279034c
Farith Palestina <palestinagodinez@gmail.com>
2023-06-22 10:33:23 -0600

    first commit

--- src/content/Lobby.jsx
+++ src/content/Lobby.jsx
@@ -1,21 +1,25 @@
+import SedansIcon from '../assets/images/icon-sedans.svg'
+import SuvsIcon from '../assets/images/icon-suvs.svg'
+import LuxuryIcon from '../assets/images/icon-luxury.svg'
+
 function Lobby(){
     return(
         <main id='allpage'>
             <div className='columns-container'>
                 <div className='first-column'>
-                    <img src='../src/assets/images/icon-sedans.svg' alt='Sedans' />
+                    <img src={SedansIcon} alt='Sedans' />
                     <h1>Sedans</h1>
                     <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
                     <button>Learn More</button>
                 </div>
                 <div className='second-column'>
-                    <img src='../src/assets/images/icon-suvs.svg' alt='Suvs' />
+                    <img src={SuvsIcon} alt='Suvs' />
                     <h1>Suvs</h1>
                     <p>Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.</p>
                     <button>Learn More</button>
                 </div>
                 <div className='third-column'>
-                    <img src='../src/assets/images/icon-luxury.svg' alt='Luxury' />
+                    <img src={LuxuryIcon} alt='Luxury' />
                     <h1>Luxury</h1>
                     <p>Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.</p>
                     <button>Learn More</button>
