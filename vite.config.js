be67249c8394d3bfc622f0257ce27ae27626cb7f
Farith Palestina <palestinagodinez@gmail.com>
2023-06-22 10:39:51 -0600

    base update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/3-column-preview-card-component-hub-vite-with-react/'
+  base: 'https://farith-palestina.github.io/3-column-preview-card-component-hub-vite-with-react'
 })
